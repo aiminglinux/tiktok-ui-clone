81972256a7db8d3f9e0cd3c24d1ef89782b4416e
Trung Thao LE <trungthao.le@officience.com>
2022-05-23 11:36:01 +0700

    Xây Dựng Logic Phần Header Dự Án Tiktok #1

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,21 +1,28 @@
-import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import classNames from "classnames/bind";
-import styles from './AccountItem.module.scss'
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import Image from '~/components/Image';
+import styles from './AccountItem.module.scss';
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 function AccountItem() {
-    return <div className={cx('wrapper')}>
-        <img className={cx('avatar')} src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/005829ed33419182ee762d8bd6814350~c5_100x100.jpeg?x-expires=1653022800&x-signature=mSLN1ArU0xzAfsEncVNRPIKXS1w%3D" alt="ThiNo" />
-        <div className={cx('info')}>
-            <h4 className={cx('name')}>
-                <span>Nguyen Thi No</span>
-                <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
-            </h4>
-            <span className={cx('username')}>nguyenthino</span>
+    return (
+        <div className={cx('wrapper')}>
+            <Image
+                className={cx('avatar')}
+                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/005829ed33419182ee762d8bd6814350~c5_100x100.jpeg?x-expires=1653022800&x-signature=mSLN1ArU0xzAfsEncVNRPIKXS1w%3D"
+                alt="ThiNo"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Thi No</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenthino</span>
+            </div>
         </div>
-    </div>
+    );
 }
 
 export default AccountItem;
